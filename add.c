7470e0eff0dc024cc311b68e3ae2245588a76475
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-10-07 23:23:45 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    printf("Please Enter The First Number\n");
+    scanf("%d", &a);
+    printf("Please Enter The Second Number\n");
+    scanf("%d", &b);
+    printf("The Sum Is %d ", a+b);
+    return 0;
+}
