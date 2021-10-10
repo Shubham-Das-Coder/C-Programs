4249ce44f08f3e4f6deb6c848562388f226845b7
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-10-10 12:07:01 +0530

    Create Pattern5.c

---
+++ Pattern5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i,j;
+    for(i=5;i>=1;i--)
+    {
+        for(j=5;j>=1;j--)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
