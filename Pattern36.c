911a8b2f52e204fd4a58f13742f98b9daba89c5e
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-10-15 20:57:25 +0530

    Create Pattern36.c

---
+++ Pattern36.c
@@ -0,0 +1,21 @@
+/*
+1
+22
+333
+4444
+55555
+*/
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
