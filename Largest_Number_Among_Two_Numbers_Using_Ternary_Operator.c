082e7f1e1a095a6893c714972261c8e6c2b66aa9
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-11-07 17:00:44 +0530

    Add files via upload

---
+++ Largest_Number_Among_Two_Numbers_Using_Ternary_Operator.c
@@ -0,0 +1,19 @@
+/* C program to find largest among
+   two numbers using ternary operator
+*/
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    // Variable declaration
+    int a, b, larg;
+    printf("Enter two number\n");
+    scanf("%d %d", &a, &b);
+
+    // Largest among a and b
+    larg = a > b ? a : b;
+
+    // Print the largest number
+    printf("largest number is : %d", larg);
+    getch();
+}
