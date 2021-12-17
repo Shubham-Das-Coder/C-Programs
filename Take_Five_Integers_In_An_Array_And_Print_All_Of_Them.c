7e79ecd225330a2754c26246ff5b5aa921a67ad8
Shubham Das <85635405+Shubham-Das-Coder@users.noreply.github.com>
2021-12-17 22:37:15 +0530

    Add files via upload

---
+++ Take_Five_Integers_In_An_Array_And_Print_All_Of_Them.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[5], i;
+    printf("Enter five numbers\n");
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 0; i < 5; i++)
+    {
+        printf("%d  ", arr[i]);
+    }
+    return 0;
+}
